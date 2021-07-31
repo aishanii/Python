b421d58c6b0431bd341f0a04fdf4b65a46897f72
Aishani Singh <84021861+aishanii@users.noreply.github.com>
2021-07-31 22:46:56 +0530

    Add files via upload

---
+++ fifteen.py
@@ -0,0 +1,12 @@
+def max_min(lst):
+    min=lst[0]
+    max=lst[0]
+    for i in lst:
+        if i>max:
+            max=i
+        elif i<min:
+            min=i
+    return max,min
+
+print(max_min([15,12,3,8]))
+
