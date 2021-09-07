89bd787a8f5e892c3ebedc048e1ca23798275311
Aishani Singh <84021861+aishanii@users.noreply.github.com>
2021-09-07 14:58:55 +0530

    aliasing

---
+++ Numpy/num.py
@@ -0,0 +1,12 @@
+#aliasing
+#only one array is created
+#both have same memory address
+
+import numpy as num
+arr1=num.array([4,7,9])
+arr2=arr1
+
+print(arr1)
+print(arr2)
+
+#both give the same output
