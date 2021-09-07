d8b560f9f022db05709e245973900812816636da
Aishani Singh <84021861+aishanii@users.noreply.github.com>
2021-09-07 13:13:54 +0530

    to get local time of greenwhich time zone

---
+++ Modules/Time/time1.py
@@ -0,0 +1,11 @@
+import datetime #greenwhich time zone
+current_time=datetime.datetime.now()
+print("The current time is:", end=" ")
+print(current_time)
+
+#timezones are defined in pytz library
+
+
+
+
+
